1d44dae5ff22cebb0611834862157c3a22b85f4f
Revidu <revidu@wireapps.co.uk>
2023-06-20 17:57:13 +0100

    Added new features

--- lib/counter_view_model.dart
+++ lib/counter_view_model.dart
@@ -1,29 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CounterViewModel extends ChangeNotifier{
 
   int _count = 0;
   int get count => _count;
+  bool isZero = false;
+  bool maxLimitReached = false;
+  double opacityOfButton = 1;
+
+  //Constructor
+  CounterViewModel() {
+    _loadCount();
+  }
+
+  // Method for loading the current count from the shared preferences instance
+  // Called in the constructor
+  void _loadCount() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _count = prefs.getInt('count') ?? 0;
+    notifyListeners();
+  }
+
+  //Method for reading current count into shared preferences
+  void _saveCount() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('count', _count);
+  }
+
+  // Checks current value of count variable and sets the
+  // other variables accordingly
+  void checkValue(){
+    if(_count == 0){
+      isZero = true;
+      opacityOfButton = 1;
+    }
+    else if(_count == 15){
+      maxLimitReached = true;
+      opacityOfButton = 0.3;
+    }
+    else{
+      isZero = false;
+      maxLimitReached = false;
+      opacityOfButton = 1;
+    }
+
+  }
 
   // adjusting the count variables
   // count can't go below 0
   void increment() {
-    if(count >= 0) {
+    if(count >= 0 && count < 15) {
       _count = _count + 1;
+      checkValue();
       notifyListeners();
+      _saveCount();
     }
-
   }
 
   void decrement() {
     if(count > 0){
       _count = _count - 1;
+      checkValue();
       notifyListeners();
+      _saveCount();
     }
+
   }
+
   void reset(){
     _count = 0;
     notifyListeners();
+    checkValue();
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:counter_app/counter_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async{
   runApp(
     ChangeNotifierProvider(
       create: (_) => CounterViewModel(),
@@ -35,67 +35,86 @@ class CounterScreen extends StatelessWidget {
     final viewModel = Provider.of<CounterViewModel>(context);
     final count = viewModel.count;
     const double buttonHeight = 50;
-    const double  buttonWidth = 200;
+    const double buttonWidth = 200;
 
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Number of times button clicked: ${viewModel.count}',
-            style: const TextStyle(fontSize: 20, color: Colors.black),
-          ),
-          const SizedBox(height: 40),
-          MaterialButton( // Button for Incrementing
-            onPressed: () {
-              viewModel.increment();
-            },
-            height: buttonHeight,
-            minWidth: buttonWidth,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Number of times button clicked: ${viewModel.count}',
+              style: const TextStyle(fontSize: 20, color: Colors.black),
             ),
-            color: Colors.blue,
-            child: const Text(
-              'Increase ++',
-              style: TextStyle(fontSize: 20, color: Colors.white),
+            const SizedBox(height: 40),
+            Opacity(
+              opacity: viewModel.opacityOfButton,
+              child: MaterialButton(
+                // Button for Incrementing
+                onPressed: () {
+                  viewModel.increment();
+                },
+                enableFeedback: !viewModel.maxLimitReached,
+                height: buttonHeight,
+                minWidth: buttonWidth,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                color: Colors.blue,
+                child: const Text(
+                  'Increase ++',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          MaterialButton( //Button for decrementing
-            onPressed: () {
-              viewModel.decrement();
-            },
-            height: 50,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
+            const SizedBox(
+              height: 20,
             ),
-            minWidth: 200,
-            color: Colors.blue,
-            child: const Text(
-              'Decrease --',
-              style: TextStyle(fontSize: 20, color: Colors.white),
+            MaterialButton(
+              //Button for decrementing
+              onPressed: () {
+                if (viewModel.isZero == true) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Error: Cannot decrement below 0'),
+                    ),
+                  );
+                }
+                viewModel.decrement();
+              },
+              height: 50,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              minWidth: 200,
+              color: Colors.blue,
+              child: const Text(
+                'Decrease --',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          MaterialButton( // Button for resetting
-            onPressed: () {
-              viewModel.reset();
-            },
-            height: 50,
-            minWidth: 200,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
+            const SizedBox(height: 20),
+            Visibility(
+              visible: !viewModel.isZero,
+              child: MaterialButton(
+                // Button for resetting
+                onPressed: () {
+                  viewModel.reset();
+                },
+                height: 50,
+                minWidth: 200,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                color: Colors.blueGrey[600],
+                child: const Text(
+                  'Reset',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
             ),
-            color: Colors.blueGrey[600],
-            child: const Text(
-              'Reset',
-              style: TextStyle(fontSize: 20, color: Colors.white),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
