246b4ac1532c9bec6f3b859ccc34495b5623c95b
Revidu <revidu@wireapps.co.uk>
2023-06-26 10:47:00 +0100

    separate colors used to a different file as static variables

---
+++ lib/Utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF37474F);
+  static const Color enabledButton = Colors.blue;
+  static const Color resetButton = Colors.blueGrey;
+  static const Color textColor = Colors.black;
+}
